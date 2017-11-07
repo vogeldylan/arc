2410e4cb6ea66e76ba3a1579ce89c4c20aa124df
vogeldylan <vogel.sohei@gmail.com>
2017-11-06 22:55:30 -0500

    Adding main.py with some notes on future functionality

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# TODO:
+#
+# - Write function to integrate with Google Calendar API and collect schedule data
+# - Create an intelligently formatted data structure to store scheduled events and important metadata about each. See the next point.
+# - Create a datastructure which stores tags/traits/past data and tie these to the above data structure.
+
+# DESIRED FUNCTIONALITY:
+# - The software should be able to return the number of 'usable' or 'productive' hours left in a given timespan, based on past behaviour (and maybe some science)
+# - The software should be able to add new events to an existing Google Calendar
+# - The software should be able to categorize inputted tasks (based on increasingly few inputs) and schedule an appropriate time for it based on past data
